d752f41e7aa2dcc6d69aa1cfc2a2f2da35b2b4fe
Eduardo Bressan Torres <jkt21000@gmail.com>
2023-08-25 09:26:22 -0300

    Bugs

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using xadrez;
 
 namespace tabuleiro
 {
@@ -19,6 +20,18 @@ namespace tabuleiro
             this.tab = tab;
             this.cor = cor;
             this.qteMovimentos = 0;
+            colocarPecas();
+        }
+
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
+
+        private void colocarPecas()
+        {
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('c', 1).toPosicao());
+            
         }
     }
 }
